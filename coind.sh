ecc6b3239d8238ea003566d7d80af8cb111f9c14
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2022-01-21 21:19:02 +0700

    Add files via upload

--- coind.sh
+++ coind.sh
@@ -1,7 +1,7 @@
 cd ~/builder/
 sudo rm -rvf compil/coind
 sudo rm -rf start.sh
-sudo wget 
+sudo wget https://github.com/arfprsty810/builder/blob/main/start.sh
 sudo chmod 777 start.sh
 clear
 bash ./start.sh
